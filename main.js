70c5b109632ddf151153de5aba7e566081ace404
Williams <wllmsawi@gmail.com>
2023-09-18 14:12:19 +0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let inputAngka1;
+let inputAngka2;
+const penambahan = (a, b) => a + b;
+let hasilTambah = penambahan(inputAngka1, inputAngka2);
+
+const pengurangan = (a, b) => a - b;
+let hasilKurang = pengurangan(inputAngka1, inputAngka2);
